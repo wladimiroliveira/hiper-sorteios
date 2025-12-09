e84469cc076a836c5eccd2ad16bd74e376b5fcd5
unknown <wladimir4019@gmail.com>
2025-12-09 10:39:55 -0300

    add `favicon.ico`

--- app/sorteios/festival-tv/layout.js
+++ app/sorteios/festival-tv/layout.js
@@ -15,6 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={`${roboto.className} antialiased`}>{children}</body>
     </html>
   );
